bc651f0743266b1408c981adcd3d08a95b8e9eb6
iamuput <111313150+iamuput@users.noreply.github.com>
2022-10-09 02:27:36 +0700

    Initial commit

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,62 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AnonX import app
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="㋱ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ ㋱",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴏᴡɴᴇʀ", user_id=OWNER),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="㋱ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ㋱",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ", user_id=OWNER),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ", url=f"{config.SUPPORT_GROUP}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                    text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url=f"{config.UPSTREAM_REPO}"
+                )
+        ],
+     ]
+    return buttons
